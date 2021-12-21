71a6c6ba05a6f75aa465510a455032c5373181d4
Dayton Bobbitt <12210193+dayton-bobbitt@users.noreply.github.com>
2021-12-21 18:55:26 -0500

    Hide player on hit

--- Player.gd
+++ Player.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal hit
+
 
 export var speed = 400 # pixels per second
 var screen_size
@@ -42,3 +44,14 @@ func _process(delta):
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
 		$AnimatedSprite.flip_h = false
+
+
+func _on_Player_body_entered(body):
+	hide()
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
